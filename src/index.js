61799f1106df009410a730c049153b7604473c53
odykyi <odykyi@luxoft.com>
2017-09-15 00:50:38 +0300

    testfv

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function sum(a, b) { return a + b; };
+module.exports = function sum(a, b) { return a - b; };
 // module.exports = {
 //   sum: function (a, b) { return a + b; },
 // }
