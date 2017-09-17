a63b2f81ca1eaa230f8b140aa8cbc7a2a2a6c63b
odykyi <odykyi@luxoft.com>
2017-09-17 16:52:21 +0300

    port

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ global.Promise = require('bluebird');
 
 const app = new IndexRoute(express);
 
-const server = app.listen(3000, () => {
-  console.log('Express app running on port %s', server.address().port);
+app.set('port', (process.env.PORT || 5000));
+
+const server = app.listen(app.get('port'), () => {
+  console.log('Express app running on port %s', app.get('port'));
 });
 
